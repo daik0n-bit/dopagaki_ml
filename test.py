519c649e22c67f16c749a122d43702fbab340a8c
daik0n-bit <daichi999518@yahoo.co.jp>
2026-08-15 12:27:15 +0900

    test.pyをGit管理から除外

--- test.py
+++
@@ -1,5 +0,0 @@
-class MyLogger:
-    def error(self, msg):
-        video_id = msg.split(": ")[0].split(" ")[1]
-        with open('data/failed_urls.txt', 'a', encoding='utf-8') as f:
-            f.write(video_id + '\n')
